f0dfaad617764c54d0abb7b3658e1bb3b31ec744
shruti.mokariya <shruti.mokariya.2024@gmail.com>
2024-05-27 09:44:11 +0530

    combined & project deleted

--- Project/middlewares/checkAdmin.js
+++
@@ -1,8 +0,0 @@
-exports.checkAdmin = async (req,res,next)=>{
-  if(req.user[0].role_id!=0){
-    return res.status(403).send("<h1>Access Denied....<h1>");
-  }
-  else{
-    next();
-  }
-}
--- Project/routes/issueBookRoutes.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const { issueBook, returnBook, getAllBookWithCount } = require("../controllers/issueBook");
-const router=express.Router();
-router.post("/issueBook",issueBook);
-router.post("/returnBook",returnBook);
-router.get("/getAllBooks",getAllBookWithCount)
-
-module.exports=router;
